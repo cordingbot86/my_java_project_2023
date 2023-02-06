8f4d9149e373604e67113d354ba8585133a0a42e
cordingbot86 <123624154+cordingbot86@users.noreply.github.com>
2023-02-06 21:09:50 +0530

    Day02
    Java basic

---
+++ src/day02/program01.java
@@ -0,0 +1,32 @@
+package day02;
+
+public class program01 {
+
+	public static void main(String[] args) {
+		System.out.println("have a nice");
+		System.out.println("day");
+		System.out.print("Happy");
+		System.out.print("holiday");
+          fun();
+	}
+ 
+    public static void fun()
+    {	
+        System.out.println("Happy birthday..");
+        int x=25;
+        char y='7';
+        boolean z=true;
+        byte a=120;
+        short b=15000;
+        long c=46576576;
+        double a1=1234.345;
+        float a2=55676.256f;
+        System.out.println(x+x);
+        System.out.println(y*y);
+        System.out.println(x*x);
+        		
+        		
+    
+    }
+  
+} 
