e02d2a4f8de3024fc85b4edba1d233e982a49709
cordingbot86 <123624154+cordingbot86@users.noreply.github.com>
2023-02-09 21:04:54 +0530

    Day04

--- src/day04/Program01.java
+++ src/day04/Program01.java
@@ -44,21 +44,20 @@ public class Program01 {
     
     
     public static void main(String[] args) {
+    	
       addMethod1(25,25);
       //subMethod1(40,10);
       mulMethod1(20,15);
       sqrtMethod1(10);
+      
       Program01 x= new Program01();
+      
       x.addMethodNonstatic(50,80);
       //x.subMethodNonstatic(20,50);
       x.mulMethodNonstatic(40,25);
       x.sqrtMethodNonstatic(30);
       
-      
-      
-      
-      
-    }
+      }
 	}
 
 
--- src/day04/Program02.java
+++ src/day04/Program02.java
@@ -3,17 +3,15 @@ package day04;
 public class Program02 extends Program01 {
 
 	public static void main(String[] args) {
-		//addMethod(25,25);//It's Private Method-not
-		//subMethos(40,10);//It's Private Method-not
-		//mulMethod(20,15);//It's Private Method-not
-		//sqrtMethod(10);//It's Private Method-not
-		Program01 x= new Program01();// current class
+		//addMethod(25,25);//It's Private Method-not visible
+		//subMethos(40,10);//It's Private Method-not visible
+		//mulMethod(20,15);//It's Private Method-not visible
+		//sqrtMethod(10);//It's Private Method-not visible
+		Program01 x= new Program01();// current class object
 		 x.addMethodNonstatic(50,80);
 	      //x.subMethodNonstatic(20,50);
 	      x.mulMethodNonstatic(40,25);
 	      x.sqrtMethodNonstatic(30);
-	      
-		
-	}
-
+	            
+}
 }
