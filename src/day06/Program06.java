cdd0c7730f976b3dfffd87e98090fe984b7b332a
cordingbot86 <123624154+cordingbot86@users.noreply.github.com>
2023-02-12 20:47:28 +0530

    Day06

---
+++ src/day06/Program06.java
@@ -0,0 +1,33 @@
+package day06;
+
+public class Program06 {
+
+	public static void main(String[] args) {
+		//+ - * / (Q) %(R)  Number
+		int a=45;
+		int b=5;
+		double c=5;
+		//int () int--->int
+		//int () double/float---->double/float
+		
+		System.out.println(a+b);
+		System.out.println(a+c);
+		
+		
+		System.out.println(a-b);
+		System.out.println(a-c);
+		
+		
+		System.out.println(a*b);
+		System.out.println(a*c);
+		
+		System.out.println(a/b);
+		System.out.println(a/c);
+		
+		System.out.println(a%b);
+		System.out.println(a%c);
+		
+		
+	}
+
+}
