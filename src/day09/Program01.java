9eb85db62dc614a76ab21ca2a022c6808bebaec2
cordingbot86 <123624154+cordingbot86@users.noreply.github.com>
2023-02-21 18:28:59 +0530

    Day09

---
+++ src/day09/Program01.java
@@ -0,0 +1,17 @@
+package day09;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		System.out.println("Strat Program....!");
+		//1-9
+		int a=1;
+		while (a<=9)//1<=9 2<=9 3<=9 4<=9 5<=9 6<=9 7<=9 8<=9 9<=9
+		{
+		System.out.println("Hello...!");//1 2 3 4 5 6 7 8 9
+        a= a+1;//2 3 4 5 6 7 8 9 10
+		}
+		System.out.println("End Program....!");
+	}
+
+}
