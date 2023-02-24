7f5d7a2e9408a53d1537046c1818acf4adcf9254
cordingbot86 <123624154+cordingbot86@users.noreply.github.com>
2023-02-24 20:38:22 +0530

    Day11

---
+++ src/day11/Program01.java
@@ -0,0 +1,26 @@
+package day11;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		/*
+		 1
+		 12
+		 123
+		 1234
+		 12345
+		 */
+		for(int i=1;i<=5;i++)//1 2 3 4 5
+		{
+			for(int j=1;j<=i;j++)
+		{
+		
+			System.out.print(j);
+		}
+		System.out.println();
+		
+			}
+		
+	}
+
+}
