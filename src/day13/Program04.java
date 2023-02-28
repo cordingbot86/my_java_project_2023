9c5008de7340409dd52548c9eacf1f71323e7e29
cordingbot86 <123624154+cordingbot86@users.noreply.github.com>
2023-02-28 09:04:15 +0530

    Day13

---
+++ src/day13/Program04.java
@@ -0,0 +1,29 @@
+package day13;
+
+public class Program04 {
+
+	public static void main(String[] args) {
+		
+		{
+			
+			int x[] = {11,12,13,14,15,16};
+			for(int i=0;i<x.length;i++)
+		
+		{
+		if(x[i]%2==0)
+		{
+			System.out.println( x[i]);
+		}
+
+	}
+	for(int i=0;i<x.length;i++)
+	{
+		if(x[i]%2!=0)
+		{
+			System.out.println(x[i]);
+		}
+	    }
+	  }
+	}
+
+}
